174ae8fa929937cbf15e76ac645103a8d4715c75
Joshua Salas <jsalaa2@gmail.com>
2023-12-20 18:58:51 -0700

    Website server is deployable

--- backend/Rest.js
+++ backend/Rest.js
@@ -3,9 +3,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const cors = require("cors");
+require('dotenv').config();
 
 // Firebase admin initialization
-var serviceAccount = require('./tox-box-firebase-adminsdk-wjobw-5f7fe6091a.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -35,9 +36,9 @@ app.post('/signup', async (req, res) => {
   const snapshot = await emailsRef.where('email', '==', userEmail).get();
   if (snapshot.empty) {
     await db.collection('emails').add({ email: userEmail });
-    res.status(200).json({ message: 'Email added to database' });
+    res.status(200).json({ message: 'Success! Your email has been added! You will receive all further updates about Tox Box.' });
   } else {
-    res.status(400).json({ message: 'Email already exists' });
+    res.status(400).json({ message: 'Email has already been added.' });
   }
 });
 
